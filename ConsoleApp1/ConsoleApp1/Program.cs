5ce35d3efbcfba48ad4cce1138fdbdc4f18b30a4
s27993 <s27993@pjwstk.edu.pl>
2024-03-13 22:31:37 +0100

    Adds FindMax Method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,3 +13,22 @@ int Multiply(int a, int b)
 }
 
 Console.WriteLine("Hey");;;
+
+static int FindMax(int[] tab)
+{
+    int max = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+    }
+
+    return max;
+}
+
+
+int[] tab = new []{1,2,-3,-8,3,16,27,1,18};
+
+Console.WriteLine(FindMax(tab));
